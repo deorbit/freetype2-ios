0b2a12ac8a09d364abe3e48834b057288b08cd37
Adam Hollidge <adam@mannedmissions.com>
2012-03-21 08:48:08 -0400

    Successful build for iOS simulator and iOS.

---
+++ Freetype2-x86/Freetype2_x86.h
@@ -0,0 +1,13 @@
+//
+//  Freetype2_x86.h
+//  Freetype2-x86
+//
+//  Created by Adam Hollidge on 3/17/12.
+//  Copyright (c) 2012 Manned Missions LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Freetype2_x86 : NSObject
+
+@end
---
+++ Freetype2-x86/Freetype2_x86.m
@@ -0,0 +1,13 @@
+//
+//  Freetype2_x86.m
+//  Freetype2-x86
+//
+//  Created by Adam Hollidge on 3/17/12.
+//  Copyright (c) 2012 Manned Missions LLC. All rights reserved.
+//
+
+#import "Freetype2_x86.h"
+
+@implementation Freetype2_x86
+
+@end
